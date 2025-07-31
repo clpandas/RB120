48617e6ea29bc3c285ea70ce63ded21d63f975aa
Crystal Lor <crystalalor@gmail.com>
2025-07-31 01:57:21 -0700

    Add more exercises. Easy 2

--- easy_2/problem7.rb
+++ easy_2/problem7.rb
@@ -1,3 +1,100 @@
+# # iteration 1
+# class Pet
+#   attr_reader :name, :type
+#   def initialize(type, name)
+#     @type = type
+#     @name = name
+#   end
+# end
+
+# class Owner
+#   attr_reader :name
+#   attr_accessor :number_of_pets
+
+#   def initialize(name)
+#     @name = name
+#     @number_of_pets = 0
+#   end
+# end
+
+# class Shelter
+#   attr_reader :client_history
+
+#   def initialize
+#     @client_history = Hash.new { |hash, key| hash[key] = [] }
+#   end
+
+#   def adopt(owner, pet)
+#     owner.number_of_pets += 1
+#     client_history[owner.name] << [pet.type, pet.name]
+#   end
+
+#   def print_adoptions
+#     client_history.each do |name, pets|
+#       puts "#{name} has adopted the following pets:"
+#       pets.each do |pair|
+#         puts "a #{pair.first} named #{pair.last}"
+#       end
+#     end
+#   end
+# end
+
+# iteration 2
+class Pet
+  attr_reader :type, :name
+  def initialize(type, name)
+    @type = type
+    @name = name
+  end
+
+  def to_s
+    "a #{type} named #{name}"
+  end
+end
+
+class Owner
+  attr_reader :name, :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+  def add_pet(pet)
+    pets << pet
+  end
+
+  def number_of_pets
+    pets.size
+  end
+
+  def print_pets
+    pets.each do |pet|
+      puts pet
+    end
+  end
+end
+
+class Shelter
+  attr_reader :client_history
+
+  def initialize
+    @client_history = []
+  end
+
+  def adopt(owner, pet)
+    owner.add_pet(pet)
+    client_history << owner unless client_history.include?(owner)
+  end
+
+  def print_adoptions
+    client_history.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
+      owner.print_pets  
+    end
+  end
+end
+
 butterscotch = Pet.new('cat', 'Butterscotch')
 pudding      = Pet.new('cat', 'Pudding')
 darwin       = Pet.new('bearded dragon', 'Darwin')
