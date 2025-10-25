f26404697a80c35bde94794263d608b8192bd902
Crystal Lor <crystalalor@gmail.com>
2025-10-24 18:02:31 -0700

    Update lesson 4

---
+++ lesson_4/hard2_q2.rb
@@ -0,0 +1,12 @@
+class Seacraft
+  include Moveable
+
+  attr_reader :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    self.fuel_efficiency = fuel_efficiency
+    self.fuel_capacity = fuel_capacity
+  end
+end
